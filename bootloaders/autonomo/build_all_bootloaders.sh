d7e8d077b30691a14df05610d13bb035a3c47429
Kees Bakker <kees@sodaq.com>
2016-12-29 21:26:01 +0100

    Add bootloader for SODAQ ONE and rebuild the others too

--- bootloaders/autonomo/build_all_bootloaders.sh
+++ bootloaders/autonomo/build_all_bootloaders.sh
@@ -4,6 +4,10 @@ make BOARD_ID=sodaq_explorer NAME=samd21_sam_ba_explorer clean all
 mkdir -p ../explorer
 mv -v samd21_sam_ba_explorer.* ../explorer/
 
+make BOARD_ID=sodaq_one SAM0_VARIANT=SAMD21G18A NAME=samd21_sam_ba_one clean all
+mkdir -p ../one
+mv -v samd21_sam_ba_one.* ../one/
+
 make BOARD_ID=sodaq_autonomo NAME=samd21_sam_ba clean all
 
 echo Done building bootloaders!
