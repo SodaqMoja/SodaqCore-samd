9163f0d55b299f02143170f93b3275fb66fe760f
Kees Bakker <kees@sodaq.com>
2017-02-01 19:51:59 +0100

    Correction of enum SAMPLE_RATE_ARITH_x8

--- cores/arduino/SERCOM.h
+++ cores/arduino/SERCOM.h
@@ -88,7 +88,7 @@ typedef enum
 {
 	SAMPLE_RATE_ARITH_x16 = 0x0,	// Arithmetic
 	SAMPLE_RATE_FRACT_x16 = 0x1,	// Fractional
-	SAMPLE_RATE_ARITH_x8 = 0x3,	// Arithmetic
+	SAMPLE_RATE_ARITH_x8 = 0x2,	// Arithmetic
 	SAMPLE_RATE_FRACT_x8 = 0x3,	// Fractional
 	SAMPLE_RATE_FRACT_x3 = 0x3,	// Arithmetic
 } SercomUartSampleRate;
