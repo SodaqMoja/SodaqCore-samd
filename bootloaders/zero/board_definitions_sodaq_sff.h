935bfd4959d62d38f8c79737ee08c2de37e316b9
Kees Bakker <kees@ijzerbout.nl>
2019-11-19 23:10:08 +0100

    SFF has its own bootloader now
    This resolves issue #6

--- bootloaders/zero/board_definitions_sodaq_sff.h
+++ bootloaders/zero/board_definitions_sodaq_sff.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2018 SODAQ.  All right reserved.
+  Copyright (c) 2019 SODAQ.  All right reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
