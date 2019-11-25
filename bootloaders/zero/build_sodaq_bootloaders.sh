3b368062cdf89b004f1559192d03c7f8cb27f232
Kees Bakker <kees@ijzerbout.nl>
2019-11-25 18:54:53 +0100

    bootloaders: do not build for variant one_no_dt
    If need be it can be build with this command:
    
        make ${DEBUG} BOARD_ID=sodaq_one SAM0_VARIANT=SAMD21G18A \
            NAME=samd21_sam_ba_one_no_dt DISABLE_DOUBLE_TAP=1 clean all

--- bootloaders/zero/build_sodaq_bootloaders.sh
+++ bootloaders/zero/build_sodaq_bootloaders.sh
@@ -11,10 +11,6 @@ make ${DEBUG} BOARD_ID=sodaq_one SAM0_VARIANT=SAMD21G18A NAME=samd21_sam_ba_one 
 mkdir -p ../one
 mv -v samd21_sam_ba_one.* ../one/
 
-make ${DEBUG} BOARD_ID=sodaq_one SAM0_VARIANT=SAMD21G18A NAME=samd21_sam_ba_one_no_dt DISABLE_DOUBLE_TAP=1 clean all
-mkdir -p ../one_no_dt
-mv -v samd21_sam_ba_one_no_dt.* ../one_no_dt/
-
 make ${DEBUG} BOARD_ID=sodaq_autonomo SAM0_VARIANT=SAMD21J18A NAME=samd21_sam_ba_autonomo clean all
 mkdir -p ../autonomo
 mv -v samd21_sam_ba_autonomo.* ../autonomo/
